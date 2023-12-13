8ef3bdb68869db19f2f96eba21374c516b747a63
josuerushanika <josuerushanika@gmail.com>
2023-12-13 14:53:39 +0200

    Update files

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+
+print('Start rock-paper-scissors game')
+
+print('Input your hand choise')
+my_hand = int(input('0:rock, 1:scissors, 2:papers'))
+computer_hand = random.randint(0, 2)
+
+ if my_hand 
